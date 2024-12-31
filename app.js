d2d2c16cb72cc5f0d9dff7498e5117fd14779f16
rodgers <rodgerstarrodgers@gmail.com>
2024-12-31 09:04:32 +0300

    add transition to slide wrappers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const menuItems = document.querySelectorAll(".menuItem");
 
 menuItems.forEach((item, index)=>{
     item.addEventListener("click", ()=>{
-      console.log("you clicked!" + index);
+      wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
     });
 });
