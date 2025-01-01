ca225c2f3ce0c0967d5f5837e96255ba4f771623
rodgers <rodgerstarrodgers@gmail.com>
2025-01-01 04:19:03 +0300

    added sizes choose option and smooth scroll

--- app.js
+++ app.js
@@ -271,3 +271,14 @@ currentProductColors.forEach((color, index) => {
     currentProductImage.src = choosenProduct.colors[index].img
   })
 })
+
+currentProductSizes.forEach((size,index)=>{
+  size.addEventListener("click",()=>{
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor= "white";
+      size.style.color= "black";
+  });
+    size.style.backgroundColor= "black";
+      size.style.color= "white";
+  });
+});
